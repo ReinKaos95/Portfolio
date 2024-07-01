b1d0da3aca89893d62f184d24e926fb00537df5e
MortuusBlasphemer <wagutierrez0707@gmail.com>
2024-07-01 16:17:59 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*Responsive menu scripting*/
+
+function responsiveMenu(){
+	var x = document.getElementById('nav');
+	if (x.className === '') {
+		x.className = 'responsive';
+	} else {
+		x.className = '';
+	}
+}
